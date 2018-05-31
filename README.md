caf7a8c2e156a3b81299e712ea737121904a6057
Abel Trotter <a_trotter0@hotmail.com>
2018-05-30 20:50:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Project Configuration Tool
+
+#### 05.30.2018
+
+#### By Abel Trotter
+
+## Description
+
+A simple bash script to create Epicodus projects and intialize the git pairs file.
+
+## Setup
+
+1. Clone the repo
+1. Run $ chmod 777 ./config-tool.sh
+1. Run $ ./config-tool.sh
+
+## Contribution Requirements
+
+1. Clone the repo
+1. Make a new branch
+1. Commit and push your changes
+1. Create a PR
+
+## Technologies Used
+
+* Bash
+* Git
+
+## Links
+
+* [Github Repo] ()
+
+## License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2018 **Abel Trotter**
---
+++ config-tool.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# A Bash script to configure the git pairs file and construct basic project structure.
+
+# Set up git pairs
+echo Do you need to set up your git pairs file? Enter y/n
+read gitPairResponse
+
+if [ $gitPairResponse == "yes" ] || [ $gitPairResponse == "y" ] || [ $gitPairResponse == "Y" ]
+then 
+  # Get user 1 data
+  echo Enter first pair initials, first name, last name, and email with spaces between each value.
+  echo example: fb Frodo Baggins frodo@shire.com
+  read user1Initials user1FirstName user1LastName user1Email
+
+  # Get user 2 data
+  echo Enter second pair initials, first name, last name, and email with spaces between each value.
+  echo example: sg Samwise Gamgee sam@shire.com
+  read user2Initials user2FirstName user2LastName user2Email
+
+  echo Creating git pairs file...
+
+  # Build pairs file
+  cd
+  touch .pairs
+  echo > .pairs
+  echo 'pairs:' >> .pairs
+  echo "  $user1Initials: $user1FirstName $user1LastName" >> .pairs
+  echo "  $user2Initials: $user2FirstName $user2LastName" >> .pairs
+  echo 'email:' >> .pairs
+  echo "  $user1Initials: $user1Email" >> .pairs
+  echo "  $user2Initials: $user2Email" >> .pairs
+
+  echo Process complete! Git pairs file created!
+fi
+
+# Set up project directory
+echo Please enter the name of the directory you want to create:
+read directoryName
+
+echo Creating directory $directoryName and building project...
+
+# Build project
+cd
+cd Desktop
+mkdir $directoryName
+cd $directoryName
+touch index.html
+mkdir css js
+touch css/styles.css js/scripts.js
+
+# Initialize git
+git init
+sleep 2
+git pair $user1Initials user2Initials
+
+# Navigate to and open project in finder
+cd ..
+open $directoryName
+echo Process complete! Your project has been created!
